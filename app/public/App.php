d031b1fbb84ac335ea5fad3ba1661ae221891cc7
Sodnom <sodnom0402@gmail.com>
2023-05-07 12:17:43 +0700

    dff

--- app/public/App.php
+++ app/public/App.php
@@ -1,19 +1,20 @@
 <?php
 
+namespace App;
+
 class App
 {
-    private function doRouting(string $uri): string {
-        if(preg_match("#/(?<route>[A-Za-z0-9-_]+)#", $uri, $matches ) and
-            file_exists("./handlers/{$matches['route']}.php")) {
+    private array $routes = [
+        'GET' => [
+            "/(?<route>[A-Za-z0-9-_]+)" => "./handlers/signin.php"
+        ],
+        'POST' => [
+            '/signin' => [UserController::class, 'signin']
 
-            return "./handlers/{$matches['route']}.php";
+        ]
+    ];
 
-        }
-
-        return "./views/NotFound.phtml";
-
-    }
-    function run(): void {
+    public function run(): void {
 
         $handler = $this->doRouting($_SERVER["REQUEST_URI"]);
 
@@ -32,6 +33,39 @@ class App
         ob_get_clean();
 
         echo $result;
+    }
+
+    private function doRouting(string $uri): string {
+        if(preg_match("#/(?<route>[A-Za-z0-9-_]+)#", $uri, $matches ) and
+            file_exists("./handlers/{$matches['route']}.php")) {
+
+            return "./handlers/{$matches['route']}.php";
+
+        }
+
+        return "./views/NotFound.phtml";
+
+    }
+
+    private function route(): ?string {
+        $uri = $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        foreach ($this->routes[$method] as $pattern => $handler) {
+
+            if (preg_match("#$pattern#", $uri, $params) {
+                if (file_exists($handler)) {
+                    return $handler;
+                }
+            }
+        }
+
+        return './views/NotFound.phtml';
+
+    }
+
+    public function addRoute(string $route, string $handlerPath, string $method): void {
+        $this->routes[$method][$route] = $handlerPath;
     }
 
 
--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,22 @@
 <?php
-include './App.php';
+
+spl_autoload_register(function ($class) {
+     $appRoot = dirname(__DIR__);
+
+      $file = str_replace("\\", DIRECTORY_SEPARATOR, $class).".php";
+      $file = preg_replace('#^App#', $appRoot, $file );
+
+      if (file_exists($file)) {
+          require $file;
+          return true;
+      }
+
+      return false;
+
+});
+
+use App\App;
+
 $app = New App();
 
 $app->run();
